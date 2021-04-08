8cdf43ae43aa4d38f0d29095c98658f18b1f68cf
SergioG <sergio.garcia02@estudiante.uam.es>
2021-04-08 17:09:05 +0200

    Add fragmentcardlist

---
+++ app/src/main/java/com/uam/proyectocards/CardListFragment.kt
@@ -0,0 +1,42 @@
+package com.uam.proyectocards
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.uam.proyectocards.databinding.FragmentCardListBinding
+
+class CardListFragment : Fragment() {
+    private lateinit var adapter: CardAdapter
+    private lateinit var binding: FragmentCardListBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding = DataBindingUtil.inflate<FragmentCardListBinding>(
+            inflater,
+            R.layout.fragment_card_list,
+            container,
+            false)
+        adapter = CardAdapter()
+        adapter.data = CardsApplication.cards
+        binding.cardRecyclerView.adapter = adapter
+
+        binding.cardsListStudyButton.setOnClickListener {
+            if(CardsApplication.numberOfCardsLeft() > 0) {
+                it.findNavController().navigate(R.id.action_cardListFragment_to_studyFragment2)
+            }
+
+            else {
+                Toast.makeText(activity, R.string.no_more_cards, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return binding.root
+    }
+}
