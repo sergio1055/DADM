9b7b1c5bb81781645985a8f3d039d9b056e1cd4b
SergioG <sergio.garcia02@estudiante.uam.es>
2021-05-03 17:24:34 +0200

    Added preferences

---
+++ app/src/main/java/com/uam/proyectocards/activities/SettingsActivity.kt
@@ -0,0 +1,53 @@
+package com.uam.proyectocards.activities
+
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
+import com.uam.proyectocards.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        PreferenceManager.setDefaultValues(
+            this,
+            R.xml.root_preferences,
+            true
+        )
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+    }
+
+    companion object {
+        val MAXIMUM_KEY = "max_number_cards"
+        val MAXIMUM_DEFAULT = "20"
+
+        fun getMaximumNumberOfCards(context: Context): String? {
+            return PreferenceManager
+                .getDefaultSharedPreferences(context)
+                .getString(MAXIMUM_KEY, MAXIMUM_DEFAULT)
+        }
+
+        fun setMaximumNumberOfCards(context: Context, max: String) {
+            val sharedPreferences = PreferenceManager
+                .getDefaultSharedPreferences(context)
+            val editor = sharedPreferences.edit()
+            editor.putString(MAXIMUM_KEY, max)
+            editor.commit ()
+        }
+    }
+}
--- app/src/main/java/com/uam/proyectocards/fragments/CardListFragment.kt
+++ app/src/main/java/com/uam/proyectocards/fragments/CardListFragment.kt
@@ -1,5 +1,6 @@
 package com.uam.proyectocards.fragments
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.*
@@ -10,6 +11,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.uam.proyectocards.R
+import com.uam.proyectocards.activities.SettingsActivity
 import com.uam.proyectocards.adapter.CardAdapter
 import com.uam.proyectocards.database.CardDatabase
 import com.uam.proyectocards.databinding.FragmentCardListBinding
@@ -88,6 +90,7 @@ class CardListFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.settings -> {
+                startActivity(Intent(activity, SettingsActivity::class.java))
             }
         }
         return super.onOptionsItemSelected(item)
